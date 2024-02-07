c38f2950302d5375524f77e41f86534c957ee2f0
Shekhar Bam <shekharbam123@gmail.com>
2024-02-07 22:00:54 +0545

    first commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-function App(){
-    return (<div>
-          <div>
-    SHEKHAR BAM
-    <hr />
-    <i>Full Stack Developer</i>
+import { useState } from "react"
+const App =() => {
+  const [count, setCount] = useState(0);
 
-  </div>
-    </div>
+  const addNumber = () => {
+    setCount(count + 1);
+  };
+
+    return (
+    <>
+   <div className="title">counter</div>
+   <div className="number">{count}</div>
+   <br />
+   <div className="add">
+    <button onClick={addNumber}>Add</button>
+   </div>
+   {count > 4 && <>good job </> }
+    </>
     );
-}
+};
 export default App;
